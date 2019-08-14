b9aacf6b68a040a744cd318b41fc4fd5fdca550e
lesuero <adriansuero@gmail.com>
2019-08-14 20:36:00 -0300

    Agregue Completo el mock de la api User

---
+++ src/main/java/Sites.java
@@ -0,0 +1,30 @@
+public class Sites {
+    private String id;
+    private String name;
+
+    public Sites() {
+
+    }
+
+
+    public Sites(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/Users.java
@@ -0,0 +1,89 @@
+public class Users {
+    private int id;
+    private String name;
+    private String username;
+    private String email;
+    private Address adress;
+    private String phone;
+    private String website;
+    private Company company;
+
+    public Users() {
+
+    }
+
+    public Users(int id, String name, String username, String email, Address adress, String phone, String website, Company company) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.adress = adress;
+        this.phone = phone;
+        this.website = website;
+        this.company = company;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Address adress) {
+        this.adress = adress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
